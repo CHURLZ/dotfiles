8c7924f38c503fdcbb9b928354d10459702ddb3f
Peter Liljenberg <pliljenberg@gmail.com>
2017-03-02 18:57:38 +0100

    updated vimrc

--- brew.sh
+++ brew.sh
@@ -11,10 +11,6 @@ brew tap caskroom/fonts
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-
 # Install some other useful utilities like `sponge`.
 brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
@@ -34,51 +30,34 @@ brew install homebrew/dupes/grep --with-default-names
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 
-brew install socat
-brew install tcpdump
-
 # Install other useful binaries.
 brew install npm
-brew install ack
-brew install dark-mode
+brew install cmake
+brew install python3
 brew install git
-brew install git-lfs
-brew install ssh-copy-id
 brew install tree
-brew install watch
-brew install unrar
+brew install docker
 brew cask install virtualbox
-brew cask install vagrant
 brew cask install slack
 brew cask install java
 brew cask install iterm2-beta
 brew cask install google-chrome
 brew cask install atom
+brew cask install lastpass
+
 # Atom plug-ins
 apm install editorconfig
 
-brew cask install tmux
-brew install reattach-to-user-namespace
 brew cask install flux
 brew cask install dropbox
-brew cask install bankid
 brew cask install spotify
-brew cask install shiftit
-brew cask install beamer
 brew cask install vlc
-brew cask install sonos
 
 brew install awscli
 brew install sbt
-brew install packer
-brew cask install visualvm
-brew install the_silver_searcher
-brew cask install font-source-code-pro
 brew cask install ngrok
 brew install jq
 brew cask install postman
-
-brew install rancher-cli
 
 # Remove outdated versions from the cellar.
 brew cleanup
